6e15e56d6f088b64a5c87fada36da405fafe0ca8
rileydevine <824055@mystma.org>
2021-10-04 09:41:24 -0500

    submitted

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 #WRITE YOUR CODE IN THIS FILE
+
+#define hasl
+def hasL(w):
+    
+    #len given word and assign to variable
+
+    for i in range(0,len(w)):
+        if w[i] == "l":
+           return True
+    return False
+
+
+    
+        
+
+
+
+
+#run function
+print(hasL("alabama"))
